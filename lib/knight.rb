686c5e038d19a541bc147e9af3e6467ef100601c
patrick neff <pneff88@gmail.com>
2022-02-07 18:02:19 -0500

    working but heavily copied

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,14 +1,23 @@
-require 'lib/board.rb'
-
 class Knight
+    attr_accessor :location, :moves, :children
 
-    def initialize
+    def initialize(location)
+        @location = location
+        @moves = possible_moves(location)
+        @children = [];
+
     end
 
-    def move(coordinates)
-
+    def possible_moves(location, result = []) #second parameter allows this to be passed down resursively
+        moves = [
+            [-1,-2], [1,2], [-1,2], [1,-2], [-2,-1], [2,1], [-2,1], [2,1]
+        ]
+        moves.each do |move|
+            x = location[0] + move[0]
+            y = location[1] + move[1]
+            result << [x,y] if x.between?(0,7) && y.between?(0,7) #makes sure we are on the chess board
+        end
+        result
     end
-
-    def move_valid?()
 
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,2 +1,10 @@
-require 'lib/board.rb'
-require 'lib/knight.rb'
+require_relative "./board.rb"
+require_relative "./knight.rb"
+
+game = Board.new #uncomment 4-11 to get the test ready
+
+game.knight_moves([3,3],[0,0])
+puts ''
+game.knight_moves([6,3],[0,0])
+puts ''
+game.knight_moves([0,0],[7,7])
